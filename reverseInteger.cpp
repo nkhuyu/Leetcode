b8342165cd04ca6d9683341dd9f4470a2d65137d
Yu Hu <nkhuyu@gmail.com>
2013-09-15 18:29:20 -0600

    fix a bug

--- reverseInteger.cpp
+++ reverseInteger.cpp
@@ -18,7 +18,8 @@ class Solution
             bool isNeg = x<0?true: false;
             int lastDigital = 0;
             int result = 0;
-            while(x>0)
+            x = abs(x);            
+            while(x > 0)
             {
                 lastDigital = x%10;
                 result = lastDigital+result*10;   
@@ -37,7 +38,7 @@ class Solution
 int main()
 {
 
-    int x=10;
+    int x = -123;
     Solution sol;
     cout<<sol.reverse(x)<<endl;
 
