36a47e43cdb705aff551e1c8fb9d6c81a7579d66
Yu Hu <nkhuyu@gmail.com>
2013-09-22 13:44:43 -0600

    solution for 3 sum

---
+++ find_triplets.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<set>
+#include<vector>
+#include<string>
+#include<vector>
+#include<algorithm>
+
+
+
+using namespace std;
+
+
+class Solution
+{
+    public:
+        set<vector<int> >find_triplets(vector<int> arr)
+        {
+            sort(arr.begin(),arr.end());
+            set<vector<int> > triplets;
+            vector<int> triplet;
+            int n = arr.size();
+
+            for(int i = 0; i < n; i++)
+            {
+                int j = i + 1;
+                int k = n - 1;
+                while (j < k)
+                {
+                    int sum_two = arr[i] + arr [j];
+                    if(sum_two + arr[k] < 0)
+                    {
+                        j++;
+                    }
+                    else if(sum_two + arr[k] > 0)
+                    {
+                        k--;
+                    }
+                    else 
+                    {
+                        triplet.push_back(arr[i]);
+                        triplet.push_back(arr[j]);
+                        triplet.push_back(arr[k]);
+                        triplets.insert(triplet);
+                        j++;
+                        k--;
+                    }
+                    
+                }
+            }
+            return triplets;
+        }
+
+};
+
+
+
+
+int main()
+{
+    vector<int> arr;
+    arr.push_back(-1);
+    arr.push_back(0);
+    arr.push_back(1);
+    arr.push_back(2); 
+    arr.push_back(-1);
+    arr.push_back(-4);
+    Solution sol;
+    sol.find_triplets(arr);
+
+    return 0;
+}
