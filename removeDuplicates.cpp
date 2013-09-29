9e520d13a69a9de6ef73b783a9204605227f4ce2
nkhuyu <nkhuyu@gmail.com>
2013-09-28 23:10:57 -0600

    remove duplicates from sorted array - fix a bug

--- removeDuplicates.cpp
+++ removeDuplicates.cpp
@@ -15,6 +15,7 @@ class Solution
     public:
         int removeDuplicates(int A[], int n)
         {
+            if(n <= 1) return n;
             int j = 0;
             int i;
             for(i = 1; i < n; i++)
