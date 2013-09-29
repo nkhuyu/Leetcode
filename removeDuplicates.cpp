3b242ae9cfb3abace1891f97f643e379f4e0d5f5
nkhuyu <nkhuyu@gmail.com>
2013-09-28 23:09:01 -0600

    remove duplicates from sorted array - create solution

---
+++ removeDuplicates.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<set>
+#include<vector>
+#include<string>
+#include<vector>
+#include<algorithm>
+
+
+
+using namespace std;
+
+
+class Solution
+{
+    public:
+        int removeDuplicates(int A[], int n)
+        {
+            int j = 0;
+            int i;
+            for(i = 1; i < n; i++)
+                {
+                    if(A[j] != A[i])
+                    {   
+                        j++;
+                        A[j] = A[i];
+                    }
+                }
+            return j+1;
+        }
+
+};
+
+
+
+
+int main()
+{
+    int A[3] = {1,1,2};
+    Solution sol;
+    cout<<sol.removeDuplicates(A,3)<<endl;
+
+    return 0;
+}
