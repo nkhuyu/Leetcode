c4b7c8b95d64dd8809b18e9516df8ca284c2fc15
Yu Hu <nkhuyu@gmail.com>
2013-10-13 11:34:41 -0600

    test vector begin end front back

--- testVectorBeginEnd.cpp
+++ testVectorBeginEnd.cpp
@@ -1,12 +1,17 @@
 #include<iostream>
 #include<vector>
+
 using namespace std;
 int main()
-vector<int> test(4,0);
-test[0] = 1;
-test[1] = 2;
-test[2] = 3;
-test[3] = 4;
+{   
+    vector<int> test(4,0);
+    test[0] = 1;
+    test[1] = 2;
+    test[2] = 3;
+    test[3] = 4;
 
-cout<<test.end()<<endl;
+    cout<<*(test.begin())<<endl;//1
+    cout<<test.front()<<endl;//1
+    cout<<test.back()<<endl;//4
+    cout<<*(test.end())<<endl;//0
 }
