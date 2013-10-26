04ff79dadbd607ef8ba1d87bda2a427927aa6647
nkhuyu@gmail.com <nkhuyu@gmail.com>
2013-10-26 12:25:09 -0600

    spiral matrix - solution

---
+++ mains/spiralOrder.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+#include<set>
+#include<vector>
+#include<string>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+template<class T>
+class Solution
+{
+    public:
+        
+        void printVector(vector<T> &s)
+        {
+            for(unsigned int i = 0; i < s.size(); i++)
+            {
+                cout<<s[i]<<" ";
+            }
+            cout<<endl;
+        }
+        
+        void printVectorVector(vector<vector<T> > &s)
+        {
+            for(unsigned int i = 0; i < s.size(); i++)
+            {
+                for(unsigned int j = 0; j < s[i].size(); j++)
+                {
+                    cout<<s[i][j]<<" ";
+                }
+                cout<<endl;
+            }
+        }
+        
+        vector<int> spiralOrder(vector<vector<int> > & matrix)
+        {
+            vector<int> results;
+            if(matrix.empty()) return results;
+            ssize_t beginX = 0, endX = matrix.size()-1;
+            ssize_t beginY = 0, endY = matrix[0].size()-1;
+            
+            // from left to right
+            for(ssize_t i = beginX; i <= endX; i++)
+                results.push_back(matrix[beginY][i]);
+            beginY++;
+            if(beginY > endY) return results;
+
+            //from top to down
+            for(ssize_t i = beginY; i <= endY; i++)
+                results.push_back(matrix[i][endX]);
+            endX--;
+            if(beginX > endX) return results;
+
+            //from right to left
+            for(ssize_t i = endX; i>=beginX; i--)
+                results.push_back(matrix[endY][i]);
+            endY--;
+            if(endY > beginY) return results;
+
+            //from bottom up
+            for(ssize_t i = endY; i>=beginY; i--)
+                results.push_back(matrix[i][beginX]);
+            beginX++;
+            if(beginX>endX) return results;
+        }
+
+};
+
+
+
+int main()
+{
+    Solution<int> sol;
+    vector<vector<int> >results;
+
+    vector<int> S;
+    sol.printVectorVector(results);
+    return 0;
+}
